95755834f1573eea2b5e894a60e84dd6719aea6d
Luca Celenza <celenzaluca@gmail.com>
2018-07-13 08:24:09 +0200

    Still struggling to get interface viewmodel rendered.

---
+++ CLSoft.MyWallet/Extensions/Helpers/HtmlHelperInterfaceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CLSoft.MyWallet.Extensions.Helpers
+{
+    public static class HtmlHelperInterfaceExtensions
+    {
+        public static IHtmlContent DisplayForInterface<TInterface, TImplementation>(this IHtmlHelper<TInterface> helper,
+            Expression<Func<TInterface, TImplementation>> expression)
+        {
+            var modelType = expression.Parameters[0].Type;
+            var expBody = expression.Body as MemberExpression;
+            var p = (PropertyInfo)expBody.Member;
+
+            var type = p.GetType();
+            var param = Expression.Parameter(type);
+            var body = new Visitor(param, type).Visit(expression.Body);
+
+            var lambda = Expression.Lambda<Func<TInterface, TImplementation>>(body, param);
+
+            return helper.DisplayFor(lambda);
+        }
+    }
+}
