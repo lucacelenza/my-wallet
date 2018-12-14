667ee28b58fbd131a8fdced5ce130154dcbad531
Luca Celenza <celenzaluca@gmail.com>
2018-12-14 17:51:46 +0100

    Addressed vulnerabilities in the lodash and minimatch dependencies; few minor fixes on Transaction/Add route.

---
+++ CLSoft.MyWallet/Mappings/Transactions/AddOrEditTransactionRequestAmountResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoMapper;
+using CLSoft.MyWallet.Models.Transactions;
+
+namespace CLSoft.MyWallet.Mappings.Transactions
+{
+    public class AddOrEditTransactionRequestAmountResolver : IValueResolver<TransactionViewModel, object, decimal>
+    {
+        private readonly IMapper _mapper;
+
+        public AddOrEditTransactionRequestAmountResolver(IMapper mapper)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public decimal Resolve(TransactionViewModel source, object destination, decimal destMember,
+            ResolutionContext context)
+        {
+            var amount = _mapper.Map<decimal>(source.Amount);
+            if (source.Type == TransactionType.Debit) amount *= -1;
+            return amount;
+        }
+    }
+}
